9af1be03a997339fba3e6fc31e85c401bf2f2f6d
madeas <an.sharapov90@gmail.com>
2019-07-15 11:33:59 +0300

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,64 @@
-const 
+const
     gulp = require('gulp'),
     pug = require('gulp-pug'),
-    rename = require("gulp-rename");
+    sourcemaps = require('gulp-sourcemaps'),
+    mmq = require('gulp-merge-media-queries'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require("gulp-rename"),
+    babel = require('gulp-babel'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
 
-gulp.task('pug-compile', function buildHTML() {
-    return gulp.src('./pug/*.pug')
+//- Stage One: task for Pug and watch
+
+gulp.task('pug', function buildHTML() {
+    return gulp.src('build/pug/pages/*.pug')
         .pipe(pug({
             pretty: true
         }))
-        .pipe(rename('./index.html'))
-        .pipe(gulp.dest('./'))
-});
+        .pipe(gulp.dest('.'))
+})
+
+//- Stage Two: connecting SASS, compile to CSS and use to PUG
+//- Use map to create sequential add
+
+gulp.task('sass', () => {
+    return gulp.src(['global.css', 'demo.css', '*.css'].map(file => `build/css/${file}`))
+        .pipe(sourcemaps.init())
+        .pipe(concat('style.css'))
+        .pipe(mmq({
+            log: true
+        }))
+        .pipe(cleanCSS()) //- minify css
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('css'))
+})
+
+//- Stage Three: concat & minify JavaScript
+//- Use map to create sequential add
+
+gulp.task('scripts', () => {
+    return gulp.src('build/js/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(concat('scripts.js'))
+        .pipe(uglify()) //- minify js
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('js'))
+})
+
+gulp.task('component-assembly', function () {
+    gulp.watch('build/pug/pages/*.pug', gulp.series('pug'))
+    gulp.watch('build/css/*.css', gulp.series('sass'))
+    gulp.watch('build/js/*.js', gulp.series('scripts'))
+})
+
+gulp.task('watch', gulp.series(gulp.parallel('pug', 'sass', 'scripts'), 'component-assembly'))
